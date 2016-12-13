2df5164e05491906419440f264ad2ac6aff7b050
giona69 <giona.granata@gmail.com>
2016-12-13 18:32:22 +0100

    Add Code Block Project

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x = 0;
+
+    int y = 0;
+
+    printf("Dammi due numeri: ");
+
+    scanf("%d", &x);
+
+    scanf("%d", &y);
+
+    printf("La moltiplicazione dei due numeri e' %d\n", x+y);
+
+    return 0;
+}
